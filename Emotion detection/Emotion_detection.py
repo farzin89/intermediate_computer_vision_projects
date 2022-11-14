0719d2dc0ea7b56072399a436a2d63cbc164c65f
farzin89 <farzin_ahmadi90@yahoo.com>
2022-11-14 17:21:34 +0100

    videocapture,convert to gray,create rectangle and convert image to array

--- Emotion detection/Emotion_detection.py
+++ Emotion detection/Emotion_detection.py
@@ -4,3 +4,10 @@ import numpy as np
 from keras.models import model_from_json
 from keras_preprocessing import image
 
+# load model
+model = model_from_json(open("fer.json","r").read())
+#load weight
+model.load_weights('fer.h5')
+# load face detection model
+face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
