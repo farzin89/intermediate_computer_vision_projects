ed5ee4f3bf70a9fd58a1bafbdfe3e05a0d2939f2
farzin89 <farzin_ahmadi90@yahoo.com>
2022-11-15 16:42:26 +0100

    write for loop for detect the face and create rectangle

--- face_detection/face_detection.py
+++ face_detection/face_detection.py
@@ -9,6 +9,18 @@ model=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 while True:
 
     ret,frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = model.detectMultiScale(gray,scaleFactor = 1.5,minNeighbors = 5)
+
+    for face in faces:
+        x1,y1,w,h = face
+        img = cv2.rectangle(frame,(x1,y1),(x1 + w, y1 + h),(0,255,0),10)
+
+
+
+
+
+
 
 
     cv2.imshow('frame',frame)
