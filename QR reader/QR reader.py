83a9e89843c3e8dd3b91ee9cd1366102d5f7dbeb
farzin89 <farzin_ahmadi90@yahoo.com>
2022-11-17 14:49:09 +0100

    find the the qr code and create rectangle

--- QR reader/QR reader.py
+++ QR reader/QR reader.py
@@ -1,10 +1,23 @@
 import cv2
 import os
 from pyzbar.pyzbar import decode
-
+import numpy as np
+import matplotlib.pyplot as plt
 input_dir = 'Capture.JPG'
 img = cv2.imread(input_dir)
 qr_info = decode(img)
 print(qr_info)
 
+data = qr_info.data
+rect = qr_info.rect
+polygon = qr_info.polygon
 
+print(data)
+print(rect)
+print(polygon)
+img = cv2.rectangle(img, (rect.left, rect.top), (rect.left + rect.width, rect.top + rect.height),
+                            (0, 255, 0), 5)
+img = cv2.polylines(img, [np.array(polygon)], True, (255, 0, 0), 5)
+
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.show()
